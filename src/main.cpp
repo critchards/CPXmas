58ce77d0c51cd4f87909bc48fccbd26716cbef36
Chris Richards <cj.richards@protonmail.com>
2022-11-13 10:11:43 -0800

    change switch action to play<->mute add playlist toggle to right button

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,13 @@
   Circuit Playground musical ornaments
   Plays a variety of Christmas and popular music on the piezo buzzer
   and flashes the 10 Neopixels of the Circuit Playground
-  C. Richards 20201110
-
   Borrowed heavily from Arduino Mario Bros Tune With Piezo Buzzer and PWM by: Dipto Pratyaksa
-  
+  Note: I had some issues with being unable to upload firmware. If that happens you have to reset the bootloader
+    at just the right time during the upload process. Watch the terminal output for when it mentions "searching for port"
+    then hit the RESET button twice to force a new com connection. If it doesnt work and you get an error about being
+    unable to connect just try uploading again and repeat until you get the timing right.
+  C. Richards 20221106 - Refactored to use Tone(), statemachines, more songs
+  C. Richards 20201110 - Initial creation, intended for charity craft fair, not complete
 */
 
 #include <Arduino.h>                      //for PlatformIO in VSCode
@@ -32,26 +35,14 @@ uint32_t christmas_colors[] = {
   0xFF0050,
 };
  
-//collection of songs from mariobrothers
+//collection of songs from games
 SONGS games[] =
 {
   {mario_melody, mario_tempo, NUMELEMENTS(mario_melody), 1200},
   {underworld_melody, underworld_tempo, NUMELEMENTS(underworld_melody), 1600},
 };
 
-//collection of christmas carols
-/* SONGS christmas[] =
-{
-  {jingle_melody, jingle_tempo, NUMELEMENTS(jingle_melody)},
-  {wish_melody, wish_tempo, NUMELEMENTS(wish_melody)},
-  {helpfallinginlove_melody, helpfallinginlove_tempo, NUMELEMENTS(helpfallinginlove_melody)},
-  {santaclaus_melody, santaclaus_tempo, NUMELEMENTS(santaclaus_melody)},
-  {jinglebellrock_melody, jinglebellrock_tempo, NUMELEMENTS(jinglebellrock_melody)},
-  {carolofthebells_melody, carolofthebells_tempo, NUMELEMENTS(carolofthebells_melody)},
-  {herecomesthesun_melody, herecomesthesun_tempo, NUMELEMENTS(herecomesthesun_melody)},
-  {melekalikimaka_melody, melekalikimaka_tempo, NUMELEMENTS(melekalikimaka_melody)}
-}; */
-
+//collection of christmas and pop songs
 SONGS christmas[] =
 {
   {jingle_melody, jingle_tempo, NUMELEMENTS(jingle_melody),2000},
@@ -63,87 +54,93 @@ SONGS christmas[] =
   {herecomesthesun_melody, herecomesthesun_tempo, NUMELEMENTS(herecomesthesun_melody), 5000}
 };
 
-bool newSong;           //flag to handle when a new song is playing
-SONGS *songPlaying;     //pointer to current playing song
-uint32_t conductor = 0; //timer for note duration
-int noteDuration;       //length of current note in ms
-int currentNote;        //frequency value of current note
+bool newSong;             //flag to handle when a new song is playing
+bool playMusic;           //flag for music, TRUE is ON, FALSE is OFF
+bool playMusic_previous;  //to check if the selection has changed
+SONGS *songPlaying;       //pointer to current playing song
+uint32_t conductor = 0;   //timer for note duration
+int noteDuration;         //length of current note in ms
+int currentNote;          //frequency value of current note
+bool songGenre = false;   //FALSE for games, TRUE for pop
+int genreButton_previous; //to handle genre toggle
+int genreButton;          //to handle genre toggle
+int buttonTimer = 0;
+int debounceTimer = 20;   //ms for debounce handling
+
 
 void setup(void)
 {
   Serial.begin(9600);
-  while (!Serial){}
+ // while (!Serial){}
   pinMode(5, OUTPUT);//buzzer
   CircuitPlayground.begin();
   CircuitPlayground.setBrightness(255);
+  newSong = true;
+  genreButton = CircuitPlayground.rightButton();
   Serial.println("Setup complete");
-  newSong = 1;
 }
 
 void loop()
 {
-/*   playSong(marioBros, 1);
-  delay(5000);
- */
-
-  //songPlaying = &marioBros[1];
-
-  if(newSong)
+  //slide on for music, off for no music
+  if(CircuitPlayground.slideSwitch())
   {
-    int trackPlaying;
-    if(CircuitPlayground.slideSwitch())
+    playMusic = true;
+    if(playMusic_previous != playMusic)     //start a new song when music is turned on
     {
-      trackPlaying = random(NUMELEMENTS(games));
-      Serial.print("Playing from games[], track ");
-      Serial.println(trackPlaying);
-      songPlaying = & games[trackPlaying];
+      newSong = true;
     }
-    else{
-      songPlaying = & christmas[random(NUMELEMENTS(christmas))];
-    }
-    //songPlaying = & christmas[6]; //to select a particular song
-    currentNote = 0;
-    noteDuration = songPlaying->beat_ms / (int )pgm_read_word_near(&(songPlaying->tempos[currentNote]));
-    newSong = 0;
+  }
+  else
+  {
+    playMusic = false;
+    newSong = true;
+  }
+  playMusic_previous = playMusic;
+
+  //button handler for song genre
+  genreButton_previous = genreButton;
+  genreButton = CircuitPlayground.rightButton();
+  if((genreButton == 1) && (genreButton_previous == 0) && ((int)millis() - buttonTimer > debounceTimer))
+  {
+    genreButton_previous = CircuitPlayground.rightButton();
+    songGenre = !songGenre;
+    newSong = true;
+    buttonTimer = millis();
+    Serial.print("switching song to genre ");
+    Serial.println(songGenre);
   }
 
-  //play the next note in the sequence only when it has been long enough since the last note started
-  if(millis() - conductor > noteDuration * 1.3)
+  //handle starting a new song
+  if(newSong)
+  {
+    if(songGenre == false)         
+    {
+      songPlaying = & games[random(NUMELEMENTS(games))];
+    }
+    else if(songGenre == true)
+    {
+      songPlaying = & christmas[random(NUMELEMENTS(christmas))];
+    }
+    //songPlaying = & christmas[6]; //to select a particular song for testing
+    currentNote = 0;
+    noteDuration = songPlaying->beat_ms / (int )pgm_read_word_near(&(songPlaying->tempos[currentNote]));
+    newSong = false;
+  }
+
+  //play the next note in the sequence only when it has been long enough since the last note started and playMusic is true
+  if(((int)(millis() - conductor) > noteDuration * 1.3)  && playMusic)
   {
     conductor = millis();
     noteDuration = songPlaying->beat_ms / (int )pgm_read_word_near(&(songPlaying->tempos[currentNote]));
-    //Serial.println((int ) pgm_read_word_near(&(songPlaying->melodies[currentNote])));
     tone(melodyPin, (int ) pgm_read_word_near(&(songPlaying->melodies[currentNote])), noteDuration);
     currentNote++;
+    //handle the end of the song
     if(currentNote > songPlaying->size)
     {
-      //Serial.println("end of song reached");
-      //Serial.println(songPlaying->size);
-      newSong = 1;
+      newSong = true;
       noTone(melodyPin);
-      delay(PACE);
+      conductor += PACE;    //add a small delay to the end of the song
     }
   }
 }
-
-/*
-//play track # from SONGS struct
-void playSong(SONGS *matrix, int track){
-  int note, tempo;
-
-
-
-
-   for (int thisNote = 0; thisNote < matrix[track].size; thisNote++) {
-    //read note and tempo from PROGMEM
-    note = (int ) pgm_read_word_near(&(matrix[track].melodies[thisNote]));
-    tempo = (int) pgm_read_word_near(&(matrix[track].tempos[thisNote]));
-
-    int noteDuration = matrix[track].beat_ms/tempo; //whole note ms duration divided by note length from tempo
-
-    tone(melodyPin, note, noteDuration);
-    int pauseBetweenNotes = noteDuration * 1.3;     //differentiates the notes. some delay is required or program gets confused?
-    delay(pauseBetweenNotes);
-    //delay(50);
-  }
-}*/
